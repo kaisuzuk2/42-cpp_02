50944f057f4bbefbc25259b92170335eeb9410ce
kaisuzuk <szkakg@gmail.com>
2025-12-25 03:13:21 +0000

    header

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -25,6 +25,24 @@ class Fixed {
         Fixed &operator=(const Fixed &);
         Fixed(const int n);
         Fixed(const float f);
+        bool operator>(const Fixed &) const;
+        bool operator<(const Fixed &) const;
+        bool operator>=(const Fixed &) const;
+        bool operator<=(const Fixed &) const;
+        bool operator==(const Fixed &) const;
+        bool operator!=(const Fixed &) const;
+
+        Fixed operator+(const Fixed &) const;
+        Fixed operator-(const Fixed &) const;
+        Fixed operator*(const Fixed &) const;
+        Fixed operator/(const Fixed &) const;
+
+        Fixed &operator++();
+        Fixed &operator++(int);
+        Fixed &operator--();
+        Fixed &operator--(int);
+
+        
         int getRawBits(void) const;
         void setRawBits(int const raw);
         float toFloat(void) const;
